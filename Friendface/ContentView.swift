f4839a2afa9ae55be95c414def41d6f8b8e6285a
sebastian.popa <sebastian.popa@softvision.com>
2024-01-07 22:14:57 +0200

    Initial Commit

---
+++ Friendface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Friendface
+//
+//  Created by sebastian.popa on 1/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by sebastian.popa on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
