30106d2dad872bf037cd503886f06093e058d56d
sebastian.popa <sebastian.popa@softvision.com>
2024-01-07 23:40:27 +0200

    Implementation of Challenge Project -> Friendface

---
+++ Friendface/Friend.swift
@@ -0,0 +1,8 @@
+//
+//  Friend.swift
+//  Friendface
+//
+//  Created by sebastian.popa on 1/7/24.
+//
+
+import Foundation
---
+++ Friendface/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Friendface
+//
+//  Created by sebastian.popa on 1/7/24.
+//
+
+import Foundation
